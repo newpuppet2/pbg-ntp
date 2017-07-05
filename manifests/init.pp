4d780bced886d410f3505e93bc595144fc0fea48
root <root@ip-172-31-30-36.ec2.internal>
2017-07-05 07:04:42 +0000

    Add module manifest and config file

---
+++ manifests/init.pp
@@ -0,0 +1,16 @@
+class ntp{
+  ensure_packages(['ntp'])
+
+file{'/etc/ntp.conf':
+    source=>'puppet:///modules/ntp/ntp.conf',
+    notify=>Service['ntp']
+    require=>Package['ntp']
+
+}
+
+service{'ntp':
+     ensure=>running,
+     enable=>true,
+}
+}
+
